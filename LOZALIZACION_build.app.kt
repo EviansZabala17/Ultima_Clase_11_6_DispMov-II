636a98f9e8a6aad14acb59b085035e8e9111fa99
EviansZabala17 <130266495+EviansZabala17@users.noreply.github.com>
2025-06-11 14:39:42 -0400

    Create LOZALIZACION_build.app.kt

---
+++ LOZALIZACION_build.app.kt
@@ -0,0 +1,14 @@
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    // Dependencia para acceder a la ubicación
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
